352f943884e35898e0319e63c4814f86fac65b16
Braqzen <103777923+Braqzen@users.noreply.github.com>
2025-03-16 23:36:12 +0000

    Update tiles

--- src/types/tiles.ts
+++ src/types/tiles.ts
@@ -18,7 +18,9 @@ export const tileOptions: TileLayerOption[] = [
   },
   {
     name: "Stadia Alidade Smooth",
-    url: "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
+    url: `https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=${
+      import.meta.env.VITE_STADIA_API_KEY
+    }`,
     minZoom: 0,
     maxZoom: 20,
     attribution:
@@ -26,25 +28,12 @@ export const tileOptions: TileLayerOption[] = [
   },
   {
     name: "Stadia Alidade Smooth Dark",
-    url: "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+    url: `https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?api_key=${
+      import.meta.env.VITE_STADIA_API_KEY
+    }`,
     minZoom: 0,
     maxZoom: 20,
     attribution:
       '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   },
-  {
-    name: "Stadia Stamen Toner",
-    url: "https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png",
-    minZoom: 0,
-    maxZoom: 20,
-    attribution:
-      '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  },
 ];
-
-// var Jawg_Dark = L.tileLayer('https://tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
-// 	attribution: '<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-// 	minZoom: 0,
-// 	maxZoom: 22,
-// 	accessToken: '<your accessToken>'
-// });
